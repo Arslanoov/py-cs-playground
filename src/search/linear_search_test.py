e659bc7bd7c7e03e7d194b618ff820c2b718ea04
Arslanoov <rasularslanoov@gmail.com>
2021-10-24 23:29:12 +0500

    feat(search): Linear search

---
+++ src/search/linear_search_test.py
@@ -0,0 +1,7 @@
+from linear_search import linear_search
+
+
+class TestLinearSearch:
+    def test_success(self):
+        assert linear_search([1, 9, 63, 143, 37, 9, 2, 6, 5], 37) == 4
+        assert linear_search([8, 90, 1, 32, 4, 6, 2], 90) == 1
